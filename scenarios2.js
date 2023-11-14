101ae7ad15e21d9359d83db22fac68fd397d0734
adiguzelt <t.adiguzel@elsevier.com>
2023-11-13 20:44:51 -0500

    change the lines

--- scenarios2.js
+++ scenarios2.js
@@ -4,7 +4,6 @@ import { check } from 'k6';
 export default function () {
     const res = http.get('https://test.k6.io/');
 
-    //Part23
     check(res, {
         'status is 200': (r) => r.status === 200
     
